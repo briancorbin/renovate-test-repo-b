3474c04bd4718de0d13a7f119922057c683d94f6
Brian Corbin <brian.william.corbin@gmail.com>
2022-09-27 17:28:31 -0700

    using submodule

--- test-b/src/main.rs
+++ test-b/src/main.rs
@@ -1,3 +1,11 @@
+use member_a;
+use member_b;
+
 fn main() {
     println!("Hello, world!");
+    let res_a = member_a::add_two_numbers(1, 2);
+    println!("adding 1 and 2: {}", res_a);
+
+    let res_b = member_b::multiply_two_numbers(4, 3);
+    println!("multiplying 4 and 3: {}", res_b);
 }
